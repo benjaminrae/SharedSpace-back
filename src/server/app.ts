42dfd883cf04086b876fec533ffe2d1516723f2d
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-21 20:34:57 +0100

    Create unknown endpoint middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import corsOptions from "./cors/corsOptions.js";
+import { generalError, unknownEndpoint } from "./middleware/errors.js";
 
 const app = express();
 
@@ -9,5 +10,8 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.disable("x-powered-by");
+
+app.use(unknownEndpoint);
+app.use(generalError);
 
 export default app;
