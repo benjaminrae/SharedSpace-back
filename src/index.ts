d78c4e2299d1fb0d242b489f2a088034d5885382
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-21 17:58:18 +0100

    Create debug and call startServer in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import { environment } from "./loadEnvironment.js";
+import debugConfig from "debug";
+import chalk from "chalk";
+import startServer from "./server/startServer.js";
+import app from "./server/app.js";
 
+const debug = debugConfig("shared-space:root");
+
+const { port } = environment;
+
+try {
+  await startServer(app, port);
+  debug(chalk.green.bold(`Server running on http://localhost:${port}`));
+} catch (error: unknown) {
+  debug(
+    chalk.red.bold(
+      `There was an error starting the server: ${(error as Error).message}`
+    )
+  );
+}
