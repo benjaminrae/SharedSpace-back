2d3d931a8399d037c562df4bb986d2d4c4b9b045
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-22 06:36:32 +0100

    Update load environments to include mongo debug and mongo uri

--- src/loadEnvironment.ts
+++ src/loadEnvironment.ts
@@ -5,8 +5,20 @@ dotenv.config();
 interface Environment {
   port: number;
   debug: string;
+  mongoDbDebug: boolean;
+  mongoDbUri: string;
 }
 
-const { PORT: port, DEBUG: debug } = process.env;
+const {
+  PORT: port,
+  DEBUG: debug,
+  MONGO_DB_DEBUG: mongoDbDebug,
+  MONGO_DB_URI: mongoDbUri,
+} = process.env;
 
-export const environment: Environment = { port: +port, debug };
+export const environment: Environment = {
+  port: +port,
+  debug,
+  mongoDbDebug: mongoDbDebug === "true",
+  mongoDbUri,
+};
