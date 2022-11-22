7cf0b8498fb7add06f6c491e58428b560ef863d1
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-22 17:14:10 +0100

    Remove startServer and connectDatabase form coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,5 +4,10 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
-  collectCoverageFrom: ["src/**/*.ts", "!src/index.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/server/startServer.ts",
+    "!src/database/connectDatabase.ts",
+  ],
 };
