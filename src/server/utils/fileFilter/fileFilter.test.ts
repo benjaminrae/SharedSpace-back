0b7ad0b5f46574829dc82d4b15d22297e2b6eb91
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-28 12:24:33 +0100

    Add tset for fileFilter with incorrect file type

--- src/server/utils/fileFilter/fileFilter.test.ts
+++ src/server/utils/fileFilter/fileFilter.test.ts
@@ -1,16 +1,51 @@
+import type { Express } from "express";
 import type { CustomRequest } from "../../middleware/auth/types";
+import { imageErrors } from "../errors";
+import httpStatusCodes from "../httpStatusCodes";
 import fileFilter from "./fileFilter";
 
+const {
+  clientErrors: { badRequestCode },
+} = httpStatusCodes;
 const req: Partial<CustomRequest> = {};
+
+const callback = jest.fn();
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("Given the function fileFilter", () => {
   describe("When it receives a custom request, no image file and a file filter callback", () => {
     test("Then it should invoke the callback with null and false", () => {
-      const callback = jest.fn();
-
       fileFilter(req as CustomRequest, undefined, callback);
 
       expect(callback).toHaveBeenCalledWith(null, false);
+    });
+  });
+
+  describe("When it receives a customRequest, a file video.mp4 and a file filter callback", () => {
+    test("Then it should invoke the callback with an imageFormatError with status 400 and message 'Wrong image format. Accepeted formats: png, jpg, jpeg, avif and webp'", () => {
+      const file: Partial<Express.Multer.File> = {
+        mimetype: "mp4",
+        originalname: "video.mp4",
+      };
+      const statusProperty = "statusCode";
+      const messageProperty = "publicMessage";
+      const expectedMessage =
+        "Wrong image format. Accepeted formats: png, jpg, jpeg, avif and webp";
+
+      fileFilter(req as CustomRequest, file as Express.Multer.File, callback);
+
+      expect(callback).toHaveBeenCalledWith(imageErrors.imageFormatError);
+      expect(imageErrors.imageFormatError).toHaveProperty(
+        messageProperty,
+        expectedMessage
+      );
+      expect(imageErrors.imageFormatError).toHaveProperty(
+        statusProperty,
+        badRequestCode
+      );
     });
   });
 });
