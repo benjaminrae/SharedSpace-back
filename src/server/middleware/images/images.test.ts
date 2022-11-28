b111a0343916b8fbd3c652fe1033cf314cd0e381
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-28 13:10:22 +0100

    Add test for supabase upload failed

--- src/server/middleware/images/images.test.ts
+++ src/server/middleware/images/images.test.ts
@@ -16,12 +16,15 @@ const req: Partial<CustomRequest> = {
 const next: NextFunction = jest.fn();
 
 describe("Given a backupImages middleware", () => {
+  const mainFile = Buffer.from("main");
+  const smallFile = Buffer.from("small");
+
   describe("When it receives a CustomRequest with a new location", () => {
     test("Then it should add the backup images to the request body and invoke next", async () => {
-      const mainFile = Buffer.from("main");
-      const smallFile = Buffer.from("small");
       const mainFileUrl = "supabase.com/main.jpg";
       const smallFileUrl = "supabase.com/small.jpg";
+      const mainFileProperty = "backup";
+      const smallFileProperty = "backupSmall";
 
       fs.readFile = jest
         .fn()
@@ -40,9 +43,26 @@ describe("Given a backupImages middleware", () => {
 
       await backupImages(req as CustomRequest, null, next);
 
-      expect(req.body.images).toHaveProperty("backup", mainFileUrl);
-      expect(req.body.images).toHaveProperty("backupSmall", smallFileUrl);
+      expect(req.body.images).toHaveProperty(mainFileProperty, mainFileUrl);
+      expect(req.body.images).toHaveProperty(smallFileProperty, smallFileUrl);
       expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives a CustomRequest with a new Location but upload to supabase fails", () => {
+    test("Then next should be called with the thrown error", async () => {
+      const error = new Error("Can't upload to Supabase");
+
+      fs.readFile = jest
+        .fn()
+        .mockResolvedValueOnce(mainFile)
+        .mockResolvedValueOnce(smallFile);
+
+      bucket.upload = jest.fn().mockRejectedValue(error);
+
+      await backupImages(req as CustomRequest, null, next);
+
+      expect(next).toHaveBeenCalledWith(error);
     });
   });
 });
