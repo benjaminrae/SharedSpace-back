bdd6d2d68eaeafd5cb5602d1c123ca2921852d9d
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-28 10:55:35 +0100

    Add multer upload middleware to locations router

---
+++ src/server/routers/locationsRouter/locationsRouter.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { addLocation } from "../../controllers/locationsControllers/locationsControllers";
+import multer from "multer";
+import paths from "../paths";
+import fileFilter from "../../utils/fileFilter";
+
+const {
+  partialPaths: { addPath },
+} = paths;
+
+const upload = multer({
+  dest: "uploads",
+  limits: {
+    fileSize: 8000000,
+  },
+  fileFilter,
+});
+
+// eslint-disable-next-line new-cap
+const locationsRouter = Router();
+
+locationsRouter.post(addPath, upload.single("image"), addLocation);
+
+export default locationsRouter;
